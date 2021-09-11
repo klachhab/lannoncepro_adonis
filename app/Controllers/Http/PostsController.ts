8c4776244cb79a191b74eb08715fce1176a27092
Khalid Lachhab <klachhab@icloud.com>
2021-09-11 10:30:16 +0100

    get Posts with specific data

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,8 +1,32 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Post from "App/Models/Post";
 
 export default class PostsController {
-  public async index ({}: HttpContextContract) {
+  public async index ({request}: HttpContextContract) {
+    const posts = await Post.query()
+        .preload('user', builder => {
+          builder.select('is_pro')
+        })
+        .preload('city',builder => {
+          builder.select('name', 'department_id')
+              .preload('department', builder => {
+                builder.select('name')
+              })
+        })
+        .preload('category', builder => {
+          builder.select('name', 'category_id')
+              .preload('parent', builder => {
+                builder.select('name')
+              })
+        })
+        .select('user_id', 'category_id', 'city_id',
+            'title', 'price', 'negotiable'
+            )
+        .paginate(request.qs().page, 10)
 
+    return {
+      posts,
+    }
   }
 
   public async create ({}: HttpContextContract) {
